036242eb09c2ba8b238773c4e6372b5d3155c561
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2018-04-16 09:43:11 +0800

    WIP: Stall Receipt print form, PrintReceipt class

---
+++ app/src/main/java/com/pylon/emarketpos/controllers/StallPrintForm.java
@@ -0,0 +1,25 @@
+package com.pylon.emarketpos.controllers;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pylon.emarketpos.R;
+
+public class StallPrintForm extends Fragment {
+    public StallPrintForm() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_stall_print_form, container, false);
+
+        return view;
+    }
+
+}
