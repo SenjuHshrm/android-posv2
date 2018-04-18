39c6dd87d826c824793a3913792f51ca6e40ffff
RG.xxvi-xxxi <rdimasapit.bos@gmail.com>
2018-04-18 13:57:25 +0800

    Ambulant print layout and print function

--- app/src/main/java/com/pylon/emarketpos/controllers/AmbulantPrintForm.java
+++ app/src/main/java/com/pylon/emarketpos/controllers/AmbulantPrintForm.java
@@ -6,11 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.pylon.emarketpos.R;
+import com.pylon.emarketpos.tasks.SavePayment;
 
-public class AmbulantPrintForm extends Fragment {
-
+public class AmbulantPrintForm extends Fragment implements View.OnClickListener{
+    private EditText OwnerName, Business, Amount;
+    private Button ambPrint;
+    private String[] SendInfo;
     public AmbulantPrintForm() {
 
     }
@@ -20,7 +26,29 @@ public class AmbulantPrintForm extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_ambulant_print_form, container, false);
+        Bundle x = getArguments();
+        OwnerName = (EditText) view.findViewById(R.id.Print_AmbName);
+        Business = (EditText) view.findViewById(R.id.Print_Business);
+        Amount = (EditText) view.findViewById(R.id.Print_Amount);
+        ambPrint = (Button) view.findViewById(R.id.btnPrintAmb);
+        OwnerName.setText(x.getString("AmbOwner"));
+        Business.setText(x.getString("AmbBusiness"));
+        ambPrint.setOnClickListener(this);
         return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        TextView DevUser = (TextView) getActivity().findViewById(R.id.DeviceUserDisplay);
+        final String pOwnerName = OwnerName.getText().toString();
+        final String pBusiness = Business.getText().toString();
+        final String pAmount = Amount.getText().toString();
+        SendInfo = new String[10];
+        SendInfo[0] = "ambulant";
+        SendInfo[1] = pOwnerName;
+        SendInfo[2] = pBusiness;
+        SendInfo[3] = pAmount;
+        SendInfo[4] = DevUser.getText().toString();
+        new SavePayment(getContext()).execute(SendInfo);
+    }
 }
